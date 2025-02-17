295929854ce5acf3c3a243c07e68afb52471e2cb
Virna <virnzlivara@gmail.com>
2025-02-17 20:25:09 +0800

    add camera feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
+ "use client";
+import Image from "next/image"; 
+import { useRouter } from "next/navigation";
+// import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const capturePhoto = () =>{
+    router.push("/takePicture");
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>TESTING</div>
+      {/* <PhotoCapture/> */}
+      <div className='flex justify-between'>
+        <button onClick={capturePhoto}>Capture Photo</button>
+        {/* <button onClick={tryAgain}>Try Again</button>
+        <button onClick={sendToEmail}>Send to Email</button> */}
+      </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
---
+++ src/app/takePicture/page.tsx
@@ -0,0 +1,12 @@
+import { PhotoCapture } from '@/components/PhotoCapture'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <PhotoCapture/>
+    </div>
+  )
+}
+
+export default page
