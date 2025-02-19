a1cf5c1e2a286b045b9072ae883d1815f36305be
Virna <virnzlivara@gmail.com>
2025-02-19 22:08:53 +0800

    fix errors

--- src/app/takePicture/page.tsx
+++ src/app/takePicture/page.tsx
@@ -1,11 +1,13 @@
 "use client"; 
 import Camera from '@/components/CameraCapture'; 
+import { QrScanner } from '@/components/QRScanner';
 import React from 'react' 
 
 const page = () => {
   return (
     <div> 
-      <Camera  />
+      {/* <Camera  /> */}
+      <QrScanner/>
     </div>
   )
 }
